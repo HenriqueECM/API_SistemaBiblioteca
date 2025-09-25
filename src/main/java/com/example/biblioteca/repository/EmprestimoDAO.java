41eca0a9be6b1172cd67746460b45657ef221a16
henriqueworks <henriqueworks68@gmail.com>
2025-09-25 17:59:43 -0300

    feat: adicionado dao emprestimo, livro e usuario

--- src/main/java/com/example/biblioteca/repository/EmprestimoDAO.java
+++ src/main/java/com/example/biblioteca/repository/EmprestimoDAO.java
@@ -17,7 +17,10 @@ public class EmprestimoDAO {
         try (Connection conn = Conexao.conectar();
              PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
 
-
+            stmt.setInt(1, emprestimo.getLivroId());
+            stmt.setInt(2, emprestimo.getUsuarioId());
+            stmt.setDate(3, Date.valueOf(emprestimo.getDataEmprestimo()));
+            stmt.setDate(4, Date.valueOf(emprestimo.getDataDevolucao()));
             stmt.executeUpdate();
 
             System.out.println("Emprestimo inserido com sucesso!");
@@ -25,7 +28,7 @@ public class EmprestimoDAO {
     }
 
     public List<Emprestimo> buscarTodos() throws SQLException{
-        String query = "SELECT id, livro_id, usuario_id, data_emprestimo, data_devolucao FROM livro";
+        String query = "SELECT id, livro_id, usuario_id, data_emprestimo, data_devolucao FROM emprestimo";
 
         List<Emprestimo> emprestimoList = new ArrayList<>();
 
@@ -34,8 +37,13 @@ public class EmprestimoDAO {
             ResultSet rs = stmt.executeQuery();
 
             while(rs.next()){
+                int id = rs.getInt("id");
+                int idLivro = rs.getInt("livro_id");
+                int idUsuario = rs.getInt("usuario_id");
+                Date dataEmprestimo = rs.getDate("data_emprestimo");
+                Date dataDevolucao = rs.getDate("data_devolucao");
 
-                Emprestimo emprestimo = new Emprestimo();
+                Emprestimo emprestimo = new Emprestimo(id, idLivro, idUsuario, dataEmprestimo.toLocalDate(), dataDevolucao.toLocalDate());
                 emprestimoList.add(emprestimo);
             }
         }
@@ -46,6 +54,10 @@ public class EmprestimoDAO {
         String query = "SELECT id, livro_id, usuario_id, data_emprestimo, data_devolucao FROM emprestimo WHERE id = ?";
 
         int newID = 0;
+        int idLivro = 0;
+        int idUsuario = 0;
+        Date dataEmprestimo = null;
+        Date dataDevolucao = null;
 
         try(Connection conn = Conexao.conectar();
             PreparedStatement stmt = conn.prepareStatement(query)){
@@ -55,19 +67,24 @@ public class EmprestimoDAO {
 
             if (rs.next()){
                 newID = rs.getInt("id");
+                idLivro = rs.getInt("livro_id");
+                idUsuario = rs.getInt("usuario_id");
+                dataEmprestimo = rs.getDate("data_emprestimo");
+                dataDevolucao = rs.getDate("data_devolucao");
 
             }
         }
-        return new Emprestimo(newID, );
+        return new Emprestimo(newID, idLivro, idUsuario, dataEmprestimo.toLocalDate(), dataDevolucao.toLocalDate());
     }
 
-    public void atualizar (Livro livro) throws SQLException {
-        String query = "UPDATE emprestimo SET  WHERE id = ?";
+    public void atualizar (int id, Date dataDevolucao) throws SQLException {
+        String query = "UPDATE emprestimo SET data_devolucao = ? WHERE id = ?";
 
         try (Connection conn = Conexao.conectar();
              PreparedStatement stmt = conn.prepareStatement(query)){
 
-
+            stmt.setDate(1, dataDevolucao);
+            stmt.setInt(2, id);
             stmt.executeUpdate();
 
             System.out.println("Emprestimo atualizado com sucesso!");
